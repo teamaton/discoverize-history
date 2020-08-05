535f2ada2ba3f9675aab4f0b5c0e946e2d6dbd4e
PiotrGasiorek <piotrjgasiorek@gmail.com>
2020-08-05 16:33:34 +0200

    Removed RWD for graphs points. Website adapts btns, overlay txt, tooltips to JSON. Added connection for cards and points

--- js/chart.js
+++ js/chart.js
@@ -1,11 +1,12 @@
 import { STYLES } from './styles.js';
 
 const CONTAINER = document.querySelector('.container').style;
-const TOGGLE_BTNS = document.querySelectorAll('.btn--toggle');
 const MILESTONES_CONTAINER = document.querySelector('.milestonesGroup');
-const CHART_SCORE = document.querySelector('.chart__score');
-const CHART_CATEGORY = document.querySelector('.chart__category');
+const CHART_OVERLAY = document.querySelectorAll('.chart__title > span');
+const BRAND_COLORS = ['#CB157D', '#0BD9AE', '#0088CC', '#F1B144'];
 let data_from_json;
+let toggle_btns;
+let active_category;
 
 // Get data for milestones
 fetch('../data/milestones.json')
@@ -20,46 +21,47 @@ fetch('../data/milestones.json')
                 </div>
             `;
         });
-    });
 
-// Nav functionality and change of styles
-TOGGLE_BTNS.forEach(btn => {
-    btn.addEventListener('click', () => {
-        
-        const UPDATE_STYLES = () => {
-            let new_color = btn.getAttribute('theme_color');
-
-            CONTAINER.background = new_color;
-            const MILESTONES = document.querySelectorAll('.milestone');
-            MILESTONES.forEach(card => {
-                card.style.backgroundColor = new_color;
-                card.style.boxShadow = `4px 4px 20px ${new_color}40`;
+        document.querySelectorAll('.milestone').forEach(card => {
+            let point_schrink;
+            let point_grow;
+            let point_index = parseInt(card.children[1].textContent) - 2007;  
+            // Turn on pulse animation
+            card.addEventListener('mouseenter',  () => {
+                myChart.options.tooltips.enabled = false; 
+                
+                point_grow = setInterval(() => {
+                    myChart.getDatasetMeta(0).data[point_index].custom = {
+                        borderWidth: 13,
+                        radius: 6
+                    };
+                    myChart.update();
+                }, 500);
+                point_schrink = setInterval(() => {
+                    myChart.getDatasetMeta(0).data[point_index].custom = {
+                        borderWidth: 6,
+                    };
+                    myChart.update();
+                }, 1000);
             });
-            
-            TOGGLE_BTNS.forEach(btn => btn.classList.remove('btn--toggle--active'));
-            btn.classList.add('btn--toggle--active');
-        }
-
-        UPDATE_STYLES();
-
-        // Display new data on graph
-        switch(btn.textContent.toLowerCase()){
-            case 'portals': 
-                myChart.data.datasets[0].data = data_from_json.portals;
-                break;
-            case 'team': 
-                myChart.data.datasets[0].data = data_from_json.team;
-                break;
-            case 'sessions': 
-                myChart.data.datasets[0].data = data_from_json.sessions;
-                break;
-            case 'inquiries': 
-                myChart.data.datasets[0].data = data_from_json.inquiries;
-                break;
-        }
-        myChart.update();
+            // Turn off pulse animation
+            card.addEventListener('mouseleave',  () => {
+                clearInterval(point_grow);
+                clearInterval(point_schrink);
+                myChart.options.tooltips.enabled = true;
+                let default_size = 6;
+                // Check if its the point marking the launch of discoverize 
+                if(point_index === 4){
+                    default_size = 13;
+                }
+                myChart.getDatasetMeta(0).data[point_index].custom = {
+                    borderWidth: default_size,
+                    pointRadius: 2
+                };
+                myChart.update();
+            });
+        })
     });
-});
 
 // Graph settings
 var ctx = document.getElementById('myChart').getContext('2d');
@@ -73,20 +75,20 @@ var myChart = new Chart(ctx, {
     options: STYLES.options
 });
 
-// RWD for points on the graph
-const ADAPT_DOTS = () => {
-    if(self.innerWidth < 600){
-        //Number - Radius of each point dot in pixels
-        myChart.data.datasets[0].pointRadius = 3;
-        //Number - Pixel width of point dot border
-        myChart.data.datasets[0].pointBorderWidth = 4;
-        //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-        myChart.data.datasets[0].pointHoverRadius = 5;
-    } else{
-        myChart.data.datasets[0].pointRadius = 3;
-        myChart.data.datasets[0].pointBorderWidth = 5;
-        myChart.data.datasets[0].pointHoverRadius = 10;
-    }
+
+const UPDATE_CHART_OVERLAY = (active_category) => {
+    let counter = 0;
+    Object.values(active_category.overlay).forEach(txt => {
+        CHART_OVERLAY[counter].textContent = txt;
+        counter++;
+    });
+}
+
+const UPDATE_CHART = () => {
+    myChart.data.datasets[0].data = active_category.data;
+    myChart.data.datasets[0].label = active_category.tooltipLabel;
+    UPDATE_CHART_OVERLAY(active_category);
+    myChart.update();
 }
 
 // Get data for the graph
@@ -95,15 +97,60 @@ fetch('../data/graph_data.json')
     .then(data => data.categories)
     .then(data => {
         data_from_json = data;
+        let color_counter = 0;
         for (let category in data_from_json) {
-            data_from_json[category].forEach(data => {
+            // Convert json to the state acceptable for chart.js
+            data_from_json[category].data.forEach(data => {
                 data.x = new Date(data.x, 1, 1);
             })
+            // Display nav according to amount and type of categories
+            document.querySelector('.btnsGroup').innerHTML += `
+                <button class='btn btn--toggle' theme_color=${BRAND_COLORS[color_counter]}>
+                    ${category}
+                </button>
+            `;
+            color_counter++;
+            if(color_counter >= BRAND_COLORS.length){
+                color_counter = 0;
+            }
         }
 
-        myChart.data.datasets[0].data = data_from_json.portals;
-        myChart.update();
-    });
+        // Make first btn active when website is loaded
+        toggle_btns = document.querySelectorAll('.btn--toggle');
+        toggle_btns[0].classList.add('btn--toggle--active');
+        // Display first data category
+        active_category = Object.values(data_from_json)[0];
+        UPDATE_CHART();
+        
+        myChart.getDatasetMeta(0).data[4].custom = {
+            borderWidth: 13,
+            pointRadius: 2
+        };
 
-window.addEventListener('resize', ADAPT_DOTS);
-window.addEventListener('load', ADAPT_DOTS);
+
+        // Nav functionality and change of styles
+        toggle_btns.forEach(btn => {
+            btn.addEventListener('click', () => {
+                
+                const UPDATE_STYLES = () => {
+                    let new_color = btn.getAttribute('theme_color');
+
+                    CONTAINER.background = new_color;
+                    const MILESTONES = document.querySelectorAll('.milestone');
+                    MILESTONES.forEach(card => {
+                        card.style.backgroundColor = new_color;
+                        card.style.boxShadow = `4px 4px 20px ${new_color}40`;
+                    });
+                    
+                    toggle_btns.forEach(btn => btn.classList.remove('btn--toggle--active'));
+                    btn.classList.add('btn--toggle--active');
+                }
+
+                // Change active category 
+                active_category = data_from_json[btn.textContent.replace(/\s/g, "")];
+                UPDATE_STYLES();
+                UPDATE_CHART();
+            });
+        });
+
+    });
